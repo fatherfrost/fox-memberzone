e02fbe37262aede892a39433a81d37b5add3e183
Oleksandr <sasha.snisarenko@ekreative.com>
2021-01-06 15:59:04 +0200

    init, cached requests to fox

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  UseInterceptors,
+  CacheInterceptor,
+  HttpService
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './authentication/local-auth.guard';
+import { AuthService } from './authentication/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private httpService: HttpService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('orders')
+  @UseInterceptors(CacheInterceptor)
+  async getOrders(@Request() req) {
+    return this.httpService.get('https://fox.exchange/orders');
+  }
+
+  @Get('pairs')
+  @UseInterceptors(CacheInterceptor)
+  async getPairs(@Request() req) {
+    return this.httpService.get('https://fox.exchange/pairs');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, HttpModule, Module, CacheInterceptor } from "@nestjs/common";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './authentication/auth.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    HttpModule,
+    CacheModule.register({
+      ttl: 60,
+      max: 100,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
